8da63c1ae8c8b782b378d7d474a129b28f9a552c
Christian Lim <christiananthonylim1@gmail.com>
2019-05-17 22:00:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,8 +66,7 @@ function removeFromCart(item) {
       return cart
       }
     }
-  console.log("That item is not in your cart.")
-  return cart
+  return("That item is not in your cart.")
 }
 
 function placeOrder(cardNumber) {
