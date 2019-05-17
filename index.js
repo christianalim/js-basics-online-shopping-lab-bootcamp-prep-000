3d4097f45b27224c3c451c9358caea917f79786f
Christian Lim <christiananthonylim1@gmail.com>
2019-05-17 19:38:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ function viewCart() {
     return ("Your shopping cart is empty.");
   } else if(cart.length === 1) {
     var one = `In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])}`
+    return one;
   }
 
 
